5a7c7c3bea5041b985b7c7ee3325845857b9ccf2
Stephen Freund <sdfreund10@gmail.com>
2017-04-24 12:48:31 -0400

    initial, poorly organized commit

--- lib/useless_methods.rb
+++ lib/useless_methods.rb
@@ -26,3 +26,22 @@ class Object
     end
   end
 end
+
+class ActiveRecord::Relation
+  def to_sequel
+    @bullshit_count ||= 0
+    @bullshit_count += 1
+    case @bullshit_count
+    when 1
+      "no"
+    when 2
+      "stahp"
+    when 3
+      "plz no"
+    when 4..(1.0/0)
+      Rails.logger.info("k fine")
+      self.to_sql
+    end
+  end
+end
+
